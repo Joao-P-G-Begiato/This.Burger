889d1623e1d6716565d13e48a3e53831bc795e5e
Fernando Patrick <fernando_lopes27@hotmail.com>
2022-07-25 22:44:27 -0400

    criação da infraestrutura do banco de dados

---
+++ src/infra/CriaEPopula.js
@@ -0,0 +1,22 @@
+// import DatabaseMetodos from "../utils/DatabaseMetodos.js";
+
+const cliente ={
+    nome : "Marcos da Silva",
+    endereco : "rua palmirinha, 222, loro jose",
+    formaDePagamento : "credito",
+    cpf : 00012311145,
+    telefone : 11981457895,
+    email : 'marcos@gmail.com'
+
+}
+try {
+
+    const tabela = await DatabaseMetodos.createTable()
+    console.log(tabela)
+
+    const criada = await DatabaseMetodos.inserirCliente(cliente)
+    console.log(criada)
+
+} catch (e) {
+    console.log(e)
+}
