a1cd39c919fa3639cad7b6af422b95aade837afa
Fernando Patrick <fernando_lopes27@hotmail.com>
2022-07-25 23:00:25 -0400

    Criação de DatabaseMetodos

--- src/infra/CriaEPopula.js
+++ src/infra/CriaEPopula.js
@@ -1,17 +1,17 @@
-// import DatabaseMetodos from "../utils/DatabaseMetodos.js";
+import DatabaseMetodos from "../utils/DatabaseMetodos.js";
 
 const cliente ={
     nome : "Marcos da Silva",
     endereco : "rua palmirinha, 222, loro jose",
     formaDePagamento : "credito",
-    cpf : 00012311145,
-    telefone : 11981457895,
+    cpf : '00012311145',
+    telefone : '11981457895',
     email : 'marcos@gmail.com'
 
 }
 try {
 
-    const tabela = await DatabaseMetodos.createTable()
+    const tabela = await DatabaseMetodos.criarTabela()
     console.log(tabela)
 
     const criada = await DatabaseMetodos.inserirCliente(cliente)
