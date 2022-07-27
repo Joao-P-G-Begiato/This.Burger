835f78b0c6cae369f94facb2f0c3edde8502e343
Fernando Patrick <fernando_lopes27@hotmail.com>
2022-07-26 21:43:59 -0400

    Criando arquivo DAOClientes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import DAOClientes from './src/DAO/DAOClientes.js'
 
 const port = process.env.PORT || 3000
 const app = express()
 
 app.listen(port, ()=>{
     console.log(`Servidor rodando em http://localhost:${port}`)
+    DAOClientes.criarTabela()
 })
 
 app.use(express.json())
