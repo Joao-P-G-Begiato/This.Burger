99a7b35dea0ab78e5ef6ae30e9393a0260676dc6
Joao P G Begiato <jpbegiato@hotmail.com>
2022-07-30 17:58:38 -0300

    criação do modelo da entidade Fornecedor

---
+++ src/models/FornecedorModel.js
@@ -0,0 +1,12 @@
+class fornecedorModel {
+    constructor(nomeFornecedor, taxa, endereco, item, telefone ) {
+        this.nomeFornecedo = nomeFornecedor,
+        this.taxa =  taxa,
+        this.endereco = endereco,
+        this.item = item, 
+        this.telefone = telefone
+    }
+}
+
+
+export default fornecedorModel
