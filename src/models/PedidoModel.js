da29f439458860d510328f8378ad5dfb6da3f02e
Lucas_Passos <lucas_passos@msn.com>
2022-07-30 16:44:55 -0300

    Criação do modelo PedidoModel.js da entidade Pedido

---
+++ src/models/PedidoModel.js
@@ -0,0 +1,11 @@
+class pedidoModel {
+    constructor(idCliente, itensPedido, quantidadeItens, totalPedido) {
+        this.idCliente = idCliente
+        this.itensPedido = itensPedido
+        this.quantidadeItens = quantidadeItens
+        this.totalPedido = totalPedido
+    }
+}
+
+
+export default pedidoModel
