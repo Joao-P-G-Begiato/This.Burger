663a02cc12cf4694f10353133046650e4acb637a
Rafael Jordão <rafaelimajordao@gmail.com>
2022-08-02 23:59:52 -0300

    Adição dos comentários nas validações de Pedido

--- src/services/ValidacaoPedido.js
+++ src/services/ValidacaoPedido.js
@@ -1,12 +1,27 @@
 export default class ValidacaoPedido {
+  /**
+   * 
+   * @param {number} idCliente 
+   * @returns boolean
+   */
   static validaIdCliente(idCliente) {
     return idCliente > 0;
   }
 
+  /**
+   * 
+   * @param {Array|string} itensPedido 
+   * @returns boolean
+   */
   static validaItensPedido(itensPedido) {
     return itensPedido ? itensPedido.length > 0 : false;
   }
 
+  /**
+   * 
+   * @param {number} quantidadeItens 
+   * @returns boolean
+   */
   static validaQuantidadeItens(quantidadeItens) {
     if (typeof quantidadeItens === "number") {
       return quantidadeItens > 0;
@@ -15,10 +30,23 @@ export default class ValidacaoPedido {
     }
   }
 
+  /**
+   * 
+   * @param {float} totalPedido 
+   * @returns boolean
+   */
   static validaTotalPedido(totalPedido) {
     return totalPedido > 0;
   }
 
+  /**
+   * 
+   * @param {number} idCliente 
+   * @param {Array|string} itensPedido 
+   * @param {number} quantidadeItens 
+   * @param {float} totalPedido 
+   * @returns boolean
+   */
   static isValid(idCliente, itensPedido, quantidadeItens, totalPedido) {
     return (
       this.validaIdCliente(idCliente) &&
